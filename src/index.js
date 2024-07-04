6d5afcfe748ed9bd3832f3adc78abed4cfbbd477
command0929 <142138531+command0929@users.noreply.github.com>
2024-07-04 18:58:42 +0900

    index.js class Added
    index.js 에 constructor 추가

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 let storage = require("./storage");
+
+function setFile /** @constructor */ (path) {
+    this.filePath = path;
+}
+
+setFile.toString = (() => "new <requireName> (string: filePath)").bind({});
+
+module.exports = setFile;
--- src/storage/core.js
+++ src/storage/core.js
@@ -7,12 +7,14 @@ module.exports = (
      * @returns {object} - value, unit
      */
     function getStorage(bytes) {
-    let unit = 0;
-    while(1024 <= bytes) {
-        bytes /= 1024, unit++;
-    }
-    return {
-        value: bytes,
-        unit: units[unit]
-    };
-}).bind({});
+        let byte = bytes;
+        let unit = 0;
+        while (1024 <= bytes) {
+            bytes /= 1024, unit++;
+        }
+        return {
+            bytes: byte,
+            value: bytes,
+            unit: units[unit]
+        };
+    }).bind({});
