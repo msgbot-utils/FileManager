11b54bf4594f556df93bc6ab3db66b828828220e
command0929 <142138531+command0929@users.noreply.github.com>
2024-07-07 13:13:05 +0900

    merge file and storage
    add file extract
    moved storage to file

---
+++ src/file/extract.js
@@ -0,0 +1,83 @@
+importPackage(java.io);
+importPackage(org.apache.commons.compress.archivers.zip);
+importPackage(org.apache.commons.compress.utils);
+
+/**
+ * 
+ * @param {string} zipFile - zip file path
+ * @param {string} toPath - extracted zip file path
+ * @param {string} password - decrypt key , not required
+ * @returns {object} - value
+ */
+
+function unzip(zipFile, toPath, password) {
+    if (!new File(zipFile).exists() || new File(zipFile).isDirectory() || !toPath) {
+        return {
+            result: false,
+            reason: "not exists or directory or not toPath",
+            path: [zipFile, toPath],
+            v: {},
+        };
+    }
+
+    try {
+        var file = new File(toPath);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        var zipFileObj = new ZipFile(new File(zipFile), password ? password.split("") : null);
+        var entries = zipFileObj.getEntries();
+        
+        while (entries.hasMoreElements()) {
+            var entry = entries.nextElement();
+            var path = toPath + File.separator + entry.getName();
+
+            if (!entry.isDirectory()) {
+                extractFile(zipFileObj, entry, path);
+            } else {
+                var dir = new File(path);
+                dir.mkdirs();
+            }
+        }
+
+        zipFileObj.close();
+
+        return {
+            result: true,
+            reason: "",
+            path: [zipFile, toPath],
+            v: {
+                zipType: new File(zipFile).getName().split(".").slice(-1).join("")
+            }
+        };
+    } catch (err) {
+        return {
+            result: false,
+            reason: "caught error while extracting",
+            path: [zipFile, toPath],
+            v: err
+        };
+    }
+}
+
+/**
+ * 
+ * @param {object} zipFileObj - zipFileObject
+ * @param {*} entry - entry
+ * @param {string} filePath - file path
+ */
+
+function extractFile(zipFileObj, entry, filePath) {
+    var bos = new BufferedOutputStream(new FileOutputStream(filePath));
+    var buffer = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+    var zipStream = zipFileObj.getInputStream(entry);
+    var len;
+    while ((len = zipStream.read(buffer)) > 0) {
+        bos.write(buffer, 0, len);
+    }
+    bos.close();
+    zipStream.close();
+}
+
+module.exports = unzip;
---
+++ src/file/storage.js
@@ -0,0 +1,54 @@
+let CORE = require("./unit.js");
+let File = java.io.File;
+
+/**
+ * 
+ * @param {string} path - directory
+ * @returns {number} - file size - bytes
+ */
+
+function storage(path) {
+    let _File = File(path);
+    if(!_File.exists()) return -1;
+    let bytesList = DirectoryReader(path);
+    let bytes = 0; bytesList.map(e => bytes += e.bytes);
+    return CORE(bytes);
+}
+
+/**
+ * 
+ * @param {string} directoryPath - directory
+ * @returns {array} - lists
+ */
+
+let DirectoryReader = /** @functions */ function (directoryPath) {
+    let directory = new File(directoryPath);
+    
+    let bytes = [];
+
+    if(directory.exists() && directory.isDirectory()) {
+        for(let file of directory.listFiles()) {
+            if(file.isFile()) {
+                let fileBytes = file.length();
+                bytes.push({
+                    path: file.toPath().toString(),
+                    name: file.getName(),
+                    bytes: fileBytes
+                });
+            }else if(file.isDirectory()) {
+                bytes = bytes.concat(DirectoryReader(file.toPath()));
+            }
+        }
+
+    } else if(directory.exists()) {
+        bytes.push({
+            path: file.toPath().toString(),
+            name: directory.getName(),
+            bytes: directory.length()
+        });
+    }
+
+    return bytes;
+}
+
+exports['getStorage'] = storage;
